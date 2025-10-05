9ed48ee3fcc137841b2a4e5c5b400f6fce81738f
umer <umery1730@gmail.com>
2025-10-05 13:27:28 +0500

    add

--- lab 1/cond.py
+++ lab 1/cond.py
@@ -16,3 +16,109 @@ elif a > b:
     print("First number is greater then second")
 else:
     print("Second number is greater hen first")
+
+
+print()    
+print("---------smallest and largest----------") 
+list = [1,2,0,33,46,82] 
+largest = max(list)
+smallest = min(list)
+print(f"Numbers in the list {list}")
+print(f"Largest numbers in the list {largest}")
+print(f"Smallest numbers in the list {smallest}")
+
+
+print()
+print("------check palindrome-------")
+str = input("Enter string: ")
+if str == str[::-1]:
+    print("yes palindrome")
+else:
+    print("No palindrome")
+
+
+print()
+print("-----count vowels------")
+word = input("Enter a word: ")
+vowels = ['a', 'e', 'i', 'o', 'u']
+found = False  
+for ch in word.lower():
+    if ch in vowels:
+        found = True
+        break
+if found:
+    print("yes")
+else:
+    print("no")
+
+
+print()
+print("--------check prime-------")
+n = int(input("Enter numbers: "))
+if n > 1:
+    for i in range(2,n):
+        if n % i == 0:
+            print("not prime")
+            break
+    else:
+            print("prime")
+else:
+    print("not prime")
+
+
+print()
+print("---------fibonacci----------")
+def fibonacci(n):
+    sequence = []
+    a,b= 0,1
+    for i in range(n):
+        sequence.append(a)
+        a,b=b,a+b
+    return sequence
+n = int(input("Ente num: "))
+print(f"Fibonacci: {fibonacci(n)}")
+
+
+
+print()
+print("---------new list---------")
+def unique(list):
+    unique_list = []
+    for item in list:
+        if item not in unique_list:
+            unique_list.append(item)
+    return unique_list
+numbers = [1, 2, 2, 3, 4, 4, 5, 1]
+print("Original list:", numbers)
+print("Unique list:", unique(numbers))
+
+print()
+print("-------frequency-------")
+sentence = input("Enter a sentence: ")
+words = sentence.split()
+frequency = {}
+for word in words:
+    word = word.lower()  
+    if word in frequency:
+        frequency[word] += 1
+    else:
+        frequency[word] = 1
+print("Word frequencies:")
+for word, count in frequency.items():
+    print(word, ":", count)
+
+
+print()
+print("--------students highest marks--------")
+students = {
+    "Ali": 85,
+    "arslan": 92,
+    "saad": 78,
+    "zain": 90,
+    "Hassan": 88
+}
+highest_student = max(students, key=students.get)
+highest_marks = students[highest_student]
+print("Students and their marks:", students)
+print(f"\nTop student: {highest_student} with {highest_marks} marks.")
+
