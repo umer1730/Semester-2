c26b0f56d50d7945dfaf47bb1e7b607976fe9ee7
umer <umery1730@gmail.com>
2025-10-19 14:38:20 +0500

    add

---
+++ lab 5/oop_4.py
@@ -0,0 +1,24 @@
+# class Session:
+#     active_sessions =0
+#     def __init__(self):
+#         Session.active_sessions +=1
+#     def __del__(self):
+#         Session.active_sessions -= 1
+# s1 = Session()
+# s2 = Session()
+# print(f"Active sessions: {Session.active_sessions}")
+class Student:
+    def __init__(self, name, roll_no, marks):
+        self.name = name
+        self.roll_no = roll_no
+        self.marks = marks
+
+    def get_grade(self):
+        if self.marks >= 80:
+            return 'A'
+        elif self.marks >= 60:
+            return 'B'
+        else:
+            return 'C'
+s1 = Student('Ayesha', 7, 85)
+print(s1.get_grade())    
