580a302572464115507e5a9de7fafb1170c6bcca
umer <umery1730@gmail.com>
2025-11-21 10:43:34 +0500

    added

---
+++ lab 6/task2.py
@@ -0,0 +1,8 @@
+class Temperature:
+    def __init__(self,c):
+        self.__celcius = c
+    def get_temp(self):
+        return 9 / 5 * self.__celcius+32
+temp = Temperature(25)
+print(temp.get_temp())
+
