eb51fcb84e05ffd0a27c4b6016e1a415558f7bcd
umer <umery1730@gmail.com>
2025-11-30 10:37:52 +0500

    add temperature class by using getter and setter

--- lab 6/task2.py
+++ lab 6/task2.py
@@ -1,7 +1,17 @@
 class Temperature:
-    def __init__(self,c):
-        self.__celcius = c
+    def __init__(self,celcius):
+        self.__celcius = celcius
+    @property
     def get_temp(self):
-        return 9 / 5 * self.__celcius+32
-temp = Temperature(25)
-print(temp.get_temp())
+        return (9/5*self.__celcius)+32
+    @get_temp.setter
+    def get_temp(self,temp):
+        if isinstance(temp,int):
+            self.__celcius = temp 
+        else:
+            print("temp cannot be negative")    
+t = Temperature(25)
+print(f"Fahrenheit: {t.get_temp}")
+t.get_temp = 35
+print(f"Fahrenheit: {t.get_temp}")
+print(t._Temperature__celcius)  #we can access private attribute
