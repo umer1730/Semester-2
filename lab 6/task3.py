834d4b3aef02ddbb23c988979f7c852758a630a0
umer <umery1730@gmail.com>
2025-11-30 10:28:46 +0500

    adding pasword manager

--- lab 6/task3.py
+++ lab 6/task3.py
@@ -1,14 +1,10 @@
 # class PasswordManager:
 #     def __init__(self,password):
 #         self.__password = password
-#     @property
-#     def get_pass(self):
-#         return self.__password 
-#     @get_pass.setter
-#     def get_pass(self,oldpass,newpass):
-#         if oldpass == self.__password:
-#             self.__password=newpass
-#             print("password updated")
+#     def set_pass(self,old_pass,new_pass):
+#         if old_pass == self.__password:
+#             self.__password = new_pass
+#             print("Password Updated")
 #         else:
 #             print("access denied")
 # pas = PasswordManager(1234)
