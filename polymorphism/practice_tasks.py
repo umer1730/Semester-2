b6050c44a7ec095b8cb6b2e6e1e5fc860ef886be
umer <umery1730@gmail.com>
2025-12-11 21:40:47 +0500

    added practice task

---
+++ polymorphism/practice_tasks.py
@@ -0,0 +1,55 @@
+class Dog:
+    def sound(self):
+        return "Bark"
+class Cat:
+    def sound(self):
+        return "Meow"
+animals = [Dog(),Cat()]
+for a in animals:
+    print(a.sound())
+
+print()
+class Bird:
+    def fly(self):
+        return "Birds can fly"
+class Penguin(Bird):
+    def fly(self):
+        return "Penguins cannot fly"
+b1 = Bird()
+b2 = Bird()
+print(b1.fly())
+print(b2.fly())
+
+print()
+class Car:
+    def move(self):
+        return "Car is driving"
+class Plane:
+    def move(self):
+        return "Plane is flying"
+def start(obj):
+    print(obj.move())
+start(Car())
+start(Plane())
+
+print()
+print(10+5)
+print("Ali"+"Agha")
+print([1,2],[3,4])
+
+print()
+print(len("Hello"))
+print(len([1,2,3,4]))
+print(len({"a":10,"b":20}))
+
+print()
+class Shape:
+    def area(self):
+        return 0
+class Square(Shape):
+    def __init__(self,side):
+        self.side  = side
+    def area(self):
+        return self.side * self.side
+s = Square(4)
+print(s.area()) 
