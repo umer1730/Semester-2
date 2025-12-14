5a3dbbae3aa78075b200e261ef62c7724b11988d
umer <umery1730@gmail.com>
2025-12-14 15:08:03 +0500

    added class person and man

--- Abstraction/class_method.py
+++ Abstraction/class_method.py
@@ -88,5 +88,29 @@ class Date:
         return cls(year, month, day)
 date = Date.from_string('2023-07-16')
 print(date.year, date.month, date.day)
-        
-    
+
+print()
+from datetime import date
+
+# random Person
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    @staticmethod
+    def from_FathersAge(name, fatherAge, fatherPersonAgeDiff):
+        return Person(name, date.today().year - fatherAge + fatherPersonAgeDiff)
+
+    @classmethod
+    def from_BirthYear(cls, name, birthYear):
+        return cls(name, date.today().year - birthYear)
+    def display(self):
+        print(self.name + "'s age is: " + str(self.age))
+class Man(Person):
+    sex = 'Female'
+man = Man.from_BirthYear('John', 1985)
+print(isinstance(man, Man))
+
+man1 = Man.from_FathersAge('John', 1965, 20)
+print(isinstance(man1, Man))
