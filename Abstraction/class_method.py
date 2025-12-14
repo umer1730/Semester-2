e79985cf6ae5db02172869a1de9116bb23b5489c
umer <umery1730@gmail.com>
2025-12-14 14:26:02 +0500

    added date class method

--- Abstraction/class_method.py
+++ Abstraction/class_method.py
@@ -76,5 +76,17 @@ Student.print_name = classmethod(Student.print_name)
 Student.print_name()
 
 print()
+class Date:
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
 
+    @classmethod
+    def from_string(cls, date_string):
+        year, month, day = map(int, date_string.split('-'))
+        return cls(year, month, day)
+date = Date.from_string('2023-07-16')
+print(date.year, date.month, date.day)
+        
     
