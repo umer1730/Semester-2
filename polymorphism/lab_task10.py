0772d42dc8fb0f18dd1d6e661cdcaaa215feb1ac
umer <umery1730@gmail.com>
2025-12-17 23:44:59 +0500

    make a complex number class

---
+++ polymorphism/lab_task10.py
@@ -0,0 +1,13 @@
+class Complex:
+    def __init__(self,real,img):
+        self.real = real
+        self.img = img
+    def __add__(self,other):
+        add_1 = self.real + other.real
+        add_2 = self.img + other.img
+        return Complex(add_1,add_2)
+    def num(self):
+        print(self.real,"i",self.img,"j")
+add_1 = Complex(1,2)
+print(add_1.num())
+
