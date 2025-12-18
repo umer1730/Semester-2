b0a4e224048afca9233a13fd45638242a7a14799
umer <umery1730@gmail.com>
2025-12-18 18:25:48 +0500

    add traceback

--- Robust code/traceback.py
+++ Robust code/traceback.py
@@ -9,17 +9,3 @@ def main():
     result = process_data(data)
     print(f"Average: {result}")
 main()
-
-
-file_handle =  None
-try:
-    file_handle = open("config.txt","r")
-    data = file_handle.read()
-except FileNotFoundError as e:
-    print(f"File not found: {e}")
-except Exception as e:
-    print(f"An error occurred: {e}")
-else:
-    print("Starting the file read process")
-finally:
-    print("Execution complete.")
