239dcf1afd344e46f9cb23c51c45e3fe11f308aa
umer <umery1730@gmail.com>
2025-12-18 18:29:17 +0500

    add file handle check

--- Robust code/error_handle.py
+++ Robust code/error_handle.py
@@ -11,4 +11,18 @@ while True:
     print("Division successfully")
     print()
 
+print()
+file_handle =  None
+try:
+    file_handle = open("config.txt","r")
+    data = file_handle.read()
+except FileNotFoundError as e:
+    print(f"File not found: {e}")
+except Exception as e:
+    print(f"An error occurred: {e}")
+else:
+    print("Starting the file read process")
+finally:
+    print("Execution complete.")
+
 
