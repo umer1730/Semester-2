fe32f46aa4ffb3e5554d38a943f739111780bb82
umer <umery1730@gmail.com>
2025-12-19 15:29:51 +0500

    add try and except

---
+++ File handling/file_1.py
@@ -0,0 +1,20 @@
+file_handle = None
+try:
+    file_handle = open("config.txt","r")
+    data = file_handle.read()
+except FileNotFoundError:
+    print("Configuration file not.Using default")
+    data = "default"
+else:
+    print("Configuration loaded successfully")
+    if "api_key" not in data:
+        print("Warning: Api key missing")
+if file_handle:
+    file_handle.close()
+    print("Cleanup complete")
+
+print()
+with open("report.txt","w") as f:
+    f.write("System Log\n")
+    f.write("Process completed successfully")
+
