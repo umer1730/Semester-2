9a50614a7c187d3f74bd1ab96b27c39590c1338c
umer <umery1730@gmail.com>
2025-12-28 12:09:24 +0500

    add appliance class

--- Robust code/all_oop.py
+++ Robust code/all_oop.py
@@ -20,3 +20,25 @@ bank = BankAccount(12000)
 bank.deposit(200)
 bank.withdraw(1000)
 print("Final Balance: ",bank.get_balance())
+
+print()
+from abc import ABC,abstractmethod
+class Appliance:
+    @abstractmethod
+    def turn_on(self):
+        pass
+class Washing_machine(Appliance):
+    def turn_on(self):
+        print("Washing machine is starting")
+        print("Locking door")
+        print("Filling wter")
+class Television(Appliance):
+    def turn_on(self):
+        print("Television is starting")
+        print("Displaying logo")
+        print("Loading channels")
+was = Washing_machine()
+was.turn_on()
+
+tv = Television()
+tv.turn_on()   
