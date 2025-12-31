d82c237ccaec7b4a678dfddd1292b0d1b24e8dab
umer <umery1730@gmail.com>
2025-12-31 22:02:49 +0500

    add age error

--- Robust code/custom_exception.py
+++ Robust code/custom_exception.py
@@ -12,3 +12,15 @@ def process_order(amount,user_balance):
     if amount <= 0:
         raise InvalidAmountError(f"${amount} is invalid")
 process_order(100,90)
+
+print()
+class AgeError(Exception):
+    pass
+def check_age(age):
+    if age < 18:
+        raise AgeError("Age ust be 18 or above")
+    return "Access granted"
+try:
+    print(check_age(15))
+except AgeError as e:
+    print(e)
