5f071e5f3dca7821a7303bdacd845b7639abd13b
umer <umery1730@gmail.com>
2026-01-01 14:38:37 +0500

    add point class for mul and sub

--- inheritance/all_practice.py
+++ inheritance/all_practice.py
@@ -81,3 +81,26 @@ cam.take_photo()
 print("\n===/ Fitness track /===")
 ft = Fitness_tracker(5)
 ft.monitor_hr()
+
+
+print()
+class Point:
+    def __init__(self,x,y):
+        self.x  =  x
+        self.y = y
+    def __mul__(self,other):
+        x =self.x * other.x
+        y =self.y * other.y
+        return x,y
+    def __sub__(self,other):
+        x = self.x - other.x
+        y = self.y - other.y
+        return x,y
+    def __str__(self):
+        return f"{self.x},{self.y}"
+p1 = Point(2,3)
+p2 = Point(4,5)
+p3 = p1*p2
+print(p3)
+p4 = p1-p2
+print(p4)
