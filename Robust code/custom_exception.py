affbd673fec00522bf8ad9e999c586c4b4e484d5
umer <umery1730@gmail.com>
2026-01-05 15:35:08 +0500

    create book class and add exception

--- Robust code/custom_exception.py
+++ Robust code/custom_exception.py
@@ -24,3 +24,21 @@ try:
     print(check_age(15))
 except AgeError as e:
     print(e)
+
+print()
+class BooksNotAvailable(Exception):
+    pass
+class Library:
+    def __init__(self):
+        self.books = {"Python": 1}
+    def borrow(self,book):
+        if self.books.get(book,0) == 0:
+            raise BooksNotAvailable("Books not available")
+        self.books[book] -= 1
+        print("book borrowed")
+lib = Library()
+try:
+    lib.borrow("Python")
+    lib.borrow("Python")
+except BooksNotAvailable as e:
+    print(e)
