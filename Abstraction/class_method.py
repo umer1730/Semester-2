d6918a8f45a3801693bbe32c9e85bbde92a05bc0
umer <umery1730@gmail.com>
2026-01-06 16:34:28 +0500

    create singleton class

--- Abstraction/class_method.py
+++ Abstraction/class_method.py
@@ -237,3 +237,14 @@ print(Company.get_info())
 person = Company.reset_count(20)
 print(person1.persons)
 print(Company.get_info())
+
+print()
+class Singleton:
+    __instance =  None
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+a = Singleton()
+b = Singleton()
+print( a is b)
