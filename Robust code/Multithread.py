edf64a791132fb4a15f7563fc199e6b8b7539305
umer <umery1730@gmail.com>
2026-01-11 09:48:18 +0500

    import sleep in multithreading

--- Robust code/Multithread.py
+++ Robust code/Multithread.py
@@ -1,14 +1,22 @@
+# In Python, we use the sleep() function (from the time module) to pause the execution of a program for a specific amount of time.
+from time import sleep
 # We use the threading module in Python to run multiple tasks at the same time within a single program.
 from threading import *     
 class Hello(Thread):
     def run(self):
         for i in range(50):
             print("Hello")
+            sleep(i)
 class Hi(Thread):
     def run(self):
         for i in range(50):
             print("Hi")
+            sleep(i)
+
 t1 = Hello()
 t2 = Hi()
+
 t1.start()
+sleep(0.2) # gap between 2 sec
 t2.start()
+
