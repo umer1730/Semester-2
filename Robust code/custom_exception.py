50e8b230ff250b3c9400a0e3f70f268d0a0394c7
umer <umery1730@gmail.com>
2026-01-14 15:28:28 +0500

    use custom exception

--- Robust code/custom_exception.py
+++ Robust code/custom_exception.py
@@ -12,7 +12,6 @@ def process_order(amount,user_balance):
     if amount <= 0:
         raise InvalidAmountError(f"${amount} is invalid")
 process_order(100,90)
-
 print()
 class AgeError(Exception):
     pass
@@ -24,7 +23,6 @@ try:
     print(check_age(15))
 except AgeError as e:
     print(e)
-
 print()
 class BooksNotAvailable(Exception):
     pass
@@ -33,12 +31,36 @@ class Library:
         self.books = {"Python": 1}
     def borrow(self,book):
         if self.books.get(book,0) == 0:
-            raise BooksNotAvailable("Books not available")
+           raise BooksNotAvailable("Books not available")
         self.books[book] -= 1
         print("book borrowed")
 lib = Library()
 try:
-    lib.borrow("Python")
-    lib.borrow("Python")
+   lib.borrow("Python")
+   lib.borrow("Python")
 except BooksNotAvailable as e:
-    print(e)
+   print(e)
+
+print()
+class SalaryReductionError(Exception):
+    pass
+class Employee:
+    def __init__(self,name,salary):
+        self.name = name
+        self.__salary = salary    
+    
+    @property
+    def salary(self):
+        return self.__salary
+    
+    @salary.setter
+    def salary(self,value):
+        if value < self.__salary:
+            raise SalaryReductionError(f"Cannot reduce salary from {self.__salary} to {value}!")
+        self.__salary = value
+        print(f"Salary updated to {self.__salary}")
+emp = Employee("ALi",10000)
+try:
+    emp.salary = 1200
+except SalaryReductionError as e:
+    print(f"Alert: {e}")
