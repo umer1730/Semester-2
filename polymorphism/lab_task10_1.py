98afdbd99121a3f5bd68d0d2ed0897552ed9b644
umer <umery1730@gmail.com>
2026-01-15 16:02:43 +0500

    use operator overloading

--- polymorphism/lab_task10_1.py
+++ polymorphism/lab_task10_1.py
@@ -10,3 +10,17 @@ class Complex:
         print(f"{self.real}i, {self.img}j")
 add_1 = Complex(9,5)
 add_1.num()
+
+
+print()
+class Vector:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def __add__(self,other):
+        return Vector(self.x+other.x,self.y+other.y)
+    def __str__(self):
+        return f"({self.x},{self.y})"
+v1 = Vector(2,3)
+v2 = Vector(4,5)
+print(v1+v2)
