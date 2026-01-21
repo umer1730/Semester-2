4ba515fdb48bd73905358138856525c4d1ca3101
umer <umery1730@gmail.com>
2026-01-21 14:37:19 +0500

    add

--- polymorphism/prac_task.py
+++ polymorphism/prac_task.py
@@ -22,3 +22,18 @@ print(p3)
 
 p4 = p1*p2
 print(p4)
+
+class CHeck:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def __floordiv__(self,other):
+        x  = self.x // other.x
+        y  = self.y // other.y
+        return x,y
+    def __str__(self):
+        return f"{self.x},{self.y}"
+p1 = CHeck(2,3)
+p2 = CHeck(6,5)
+p3 = p1 // p2
+print(p3)
