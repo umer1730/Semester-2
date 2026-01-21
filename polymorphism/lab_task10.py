e2c75d17b5acd70de22d987c6ca0ae7d98007104
umer <umery1730@gmail.com>
2026-01-21 15:09:11 +0500

    create travellig company

--- polymorphism/lab_task10.py
+++ polymorphism/lab_task10.py
@@ -10,3 +10,33 @@ class Car:
 
 car = Car() 
 car.start() 
+
+print()
+class Aeroplane:
+    Company = "AirSial"
+    def __init__(self,name):
+        self.name = name
+    def display_info(self):
+        print(f"Name: {self.name}")
+class Employee(Aeroplane):
+    def __init__(self,name,role,salary):
+        self.name = name
+        self.role =role
+        self.__salary = salary
+    def display_info(self):
+        print(f"Name: {self.name} \nRole: {self.role} \nSalary: {self.__salary}")
+class Passenger(Aeroplane):
+    def __init__(self,name,seat_no):
+        self.name = name
+        self.seat_no = seat_no
+    def display_info(self):
+        print(f"Name: {self.name} \nSeat No: {self.seat_no}")
+
+p = Passenger("ALi",12)
+p.display_info()
+
+e = Employee("Ahmed","SPO",100000)
+e.display_info()
+
+a = Aeroplane("asvchs")
+a.display_info()
