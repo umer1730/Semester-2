f5aec9097fa8efe95919877e4f0ca57f0e681894
umer <umery1730@gmail.com>
2026-02-07 19:01:28 +0500

    read student.txt file

--- File handling/lab_pract.py
+++ File handling/lab_pract.py
@@ -11,3 +11,8 @@ print()
 with open("student.txt","a") as f:
     name = input("Enter name: ")
     f.write(f"{name} is a good student.He work hard daily and do every works at a regualr time")
+
+print()
+with open("student.txt","r") as f:
+    data = f.read()
+    print(data)
