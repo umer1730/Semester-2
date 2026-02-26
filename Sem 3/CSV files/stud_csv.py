ca0885b444bf1925f28034a6ece887e2692bc4e7
Umer Yaseen <umery1730@gmail.com>
2026-02-26 16:07:39 +0500

    add

--- Sem 3/CSV files/stud_csv.py
+++ Sem 3/CSV files/stud_csv.py
@@ -9,3 +9,22 @@ with open("D:\\Sem 2\\Sem 3\\CSV files\\stud.csv","r") as f:
 
         print()
         print(row[0]) #print names only
+
+        age = int(row[1])
+        print(age)
+
+    
+
+
+
+
+
+
+# import csv
+
+# with open("new_students.csv", "w", newline="") as file:
+#     writer = csv.writer(file)
+    
+#     writer.writerow(["Name", "Age", "City"])
+#     writer.writerow(["Usman", 21, "Lahore",6.0])
+#     writer.writerow(["Ali", 22, "Karachi",5.2])
