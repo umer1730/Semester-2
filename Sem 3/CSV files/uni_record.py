19efa51b2ff51de6e07f5d33c00f915431d559f5
Umer Yaseen <umery1730@gmail.com>
2026-03-02 15:33:16 +0500

    checck uni data row

---
+++ Sem 3/CSV files/uni_record.py
@@ -0,0 +1,19 @@
+import csv
+
+# Define header and data rows
+fields = ['Name', 'Branch', 'Year', 'CGPA']
+rows = [
+    ['Nikhil', 'COE', '2', '9.0'],
+    ['Sanchit', 'COE', '2', '9.1'],
+    ['Aditya', 'IT', '2', '9.3'],
+    ['Sagar', 'SE', '1', '9.5'],
+    ['Prateek', 'MCE', '3', '7.8'],
+    ['Sahil', 'EP', '2', '9.1']
+]
+
+filename = "university_records.csv"
+with open(filename, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(fields)             # Write header
+    csvwriter.writerows(rows)
+    
