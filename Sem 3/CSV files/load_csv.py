dc7973d28993f5462ad464e460246bc55b6d4c53
Umer Yaseen <umery1730@gmail.com>
2026-03-06 13:28:25 +0500

    load data

---
+++ Sem 3/CSV files/load_csv.py
@@ -0,0 +1,11 @@
+import csv
+def read_csv(filename,has_header=True):
+    with open(filename,"r",newline = "",encoding="utf-8")as f:
+        reader = csv.reader(f)
+        if has_header:
+            header = next(reader)
+            data = list(reader)
+            return header,data
+        
+        return list(reader)
+print(read_csv("stud.csv"))
