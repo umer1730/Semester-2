dddd2b89335cb6ee6d06bdb99959e0584a63cef6
Umer Yaseen <umery1730@gmail.com>
2026-03-06 20:51:23 +0500

    add

---
+++ Sem 3/Numpy/program.py
@@ -0,0 +1,20 @@
+import numpy as np
+data = np.array([[1.5, -0.1, 3],[0,-3,6.5]])
+print(data)
+
+print()
+print("ndim")
+print(data.ndim)
+
+print()
+print("shape")
+print(data.shape)
+
+print()
+print("size")
+print(data.size)
+
+print()
+print("dtype")
+print(data.dtype)
+
