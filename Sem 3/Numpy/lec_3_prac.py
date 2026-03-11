4d6676063bf2193ad701bd68c2fa14ee79d1f5e2
Umer Yaseen <umery1730@gmail.com>
2026-03-12 04:05:57 +0500

    transpose matrix

--- Sem 3/Numpy/lec_3_prac.py
+++ Sem 3/Numpy/lec_3_prac.py
@@ -11,11 +11,19 @@ print(A / B)
 
 print()
 print("infix operator")
-A = np.array([1,2],[3,4])
-B = np.array([5,6],[3,2])
-C = A @ B
+A = np.array([[1,2],[3,4]])
+B = np.array([[5,6],[3,2]])
+C = A @ B # np.do()
 print(C)
 
+print()
+print("transposing matrix")
+arr = np.array([[1,2,3],[4,5,6]])
 
+print(arr.shape)
+# transpose it
+print(arr.T)
+# transpose shape
+print(arr.T.shape)
 
 
