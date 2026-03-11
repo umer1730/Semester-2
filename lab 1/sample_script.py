9321147cdd473ae324479852affbb434c903b7b3
Umer Yaseen <umery1730@gmail.com>
2026-03-11 12:40:07 +0500

    sample module

---
+++ lab 1/sample_script.py
@@ -0,0 +1,9 @@
+import sample_module
+#from sample_module import city
+#from sample_module import greet
+#print(city)
+#print(greet("Ali"))
+
+#A module is used to organize and reuse code
+print(sample_module.city)
+print(sample_module.greet("Ali"))
