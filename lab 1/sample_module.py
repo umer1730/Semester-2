18ecee9141a5183e8d329d0b9856b2b5fb185651
Umer Yaseen <umery1730@gmail.com>
2026-03-11 12:40:31 +0500

    sample data

---
+++ lab 1/sample_module.py
@@ -0,0 +1,11 @@
+city = "Lahore"
+
+def greet(first_name):
+    return f"hello ,{first_name}"  #A script is used to run a program directly
+#print(greet("Ali"))
+
+if __name__ == "__main__":
+    print(city)  
+    print(greet("Ali"))
+else:
+    print("Processing")  
