4b21f16229af18356a7a49f56d4d06ae0f24332a
Umer Yaseen <umery1730@gmail.com>
2026-03-16 03:25:44 +0500

    indexing

--- Sem 3/Numpy/practice_num.py
+++ Sem 3/Numpy/practice_num.py
@@ -30,3 +30,14 @@ print(arr[0])
 print(arr[4])
 print(arr[1:4])
 print(arr[::-1])
+
+print()
+b= np.array([[1,2,3],
+             [4,5,6],
+             [7,8,9]])
+print(b[2, 1])
+print(b[0])
+print(b[:,1])
+print(b[1:3, 1:3])
+
+
