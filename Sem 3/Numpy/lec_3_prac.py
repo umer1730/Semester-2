956c79d2ff37e1b9544bcf0761e6dfe34431736f
Umer Yaseen <umery1730@gmail.com>
2026-03-15 19:02:47 +0500

    use solve and det by using lin alg method

--- Sem 3/Numpy/lec_3_prac.py
+++ Sem 3/Numpy/lec_3_prac.py
@@ -33,3 +33,20 @@ X = np.array([[1,2],[3,4]])
 
 X_inv = inv(X)
 print(X @ X_inv)
+
+print()
+print("Linalg solve")
+# A represents the coefficients (the numbers in front of x and y)
+A = np.array([[1, 1], [2, 4]])
+# b represents the constants (the results of the equations)
+b = np.array([5, 16])
+# Solve for x and y
+solution = np.linalg.solve(A, b)
+print(solution) 
+
+det = np.linalg.det(A)
+print(det) 
+
+eigvals, eigvecs = np.linalg.eig(A)
+print("Eigenvalues:", eigvals)
+print("Eigenvectors:\n", eigvecs)
