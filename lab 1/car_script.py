b45651332df07680f42b81b2ee05c999b5d7213a
UMER YASEEN <umery1730@gmail.com>
2026-03-22 13:26:16 +0500

    car model

---
+++ lab 1/car_script.py
@@ -0,0 +1,9 @@
+car_model1 = "BMW 335i"
+car_model2 = "Jeep Wrangler Rubicon"
+
+car_year1 = 2008
+car_year2 = 2021
+
+if __name__ == "__main__":
+    print("Car1: ",car_year1,car_model1)
+    print("Car2: ",car_year2,car_model2)
