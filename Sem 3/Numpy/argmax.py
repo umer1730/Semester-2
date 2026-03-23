e665a3dd384a7b9338a9339bc7f491f99595bb31
UMER YASEEN <umery1730@gmail.com>
2026-03-23 08:38:30 +0500

    matrix argument

---
+++ Sem 3/Numpy/argmax.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+A = np.array([
+    [3, 8, 5, 2],
+    [7, 1, 6, 4],
+    [9, 3, 5, 11]
+])
+
+mask = (A % 2 == 0)
+A_masked = np.where(mask, A, -np.inf)
+
+indices = np.argmax(A_masked, axis=1)
+
+print(indices)
