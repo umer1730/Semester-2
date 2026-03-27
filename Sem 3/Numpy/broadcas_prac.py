676c314fa5ccee59b1817eb7084bac126c17363e
UMER YASEEN <umery1730@gmail.com>
2026-03-28 01:15:42 +0500

    row broadcasting

--- Sem 3/Numpy/broadcas_prac.py
+++ Sem 3/Numpy/broadcas_prac.py
@@ -16,3 +16,8 @@ A = np.array([1,2,3])
 B = np.array([4,5,6])
 print(A + B)
 
+print()
+print("Add 1D array to 2D array")
+A = np.array([[10,20,30],[40,50,60],[70,80,90]])
+B = np.array([1,2,3])
+print(A + B)
