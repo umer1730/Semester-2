9900b7fb0a7b137d0a67b6a1f3f55e814536bb35
UMER YASEEN <umery1730@gmail.com>
2026-03-28 01:18:12 +0500

    Column broadcasting

--- Sem 3/Numpy/broadcas_prac.py
+++ Sem 3/Numpy/broadcas_prac.py
@@ -21,3 +21,10 @@ print("Add 1D array to 2D array")
 A = np.array([[10,20,30],[40,50,60],[70,80,90]])
 B = np.array([1,2,3])
 print(A + B)
+
+
+print()
+print("Add column vector to matrix")
+A = np.array([[1,2,3],[4,5,6],[7,8,9]])
+B = np.array([10,20,30]).reshape(3,1)
+print(A+ B)
