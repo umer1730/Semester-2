0799d57603789aa2e24734c7a1c9c1ba42e6ecbe
UMER YASEEN <umery1730@gmail.com>
2026-03-28 01:13:20 +0500

    multiply matrix by scalar

--- Sem 3/Numpy/broadcas_prac.py
+++ Sem 3/Numpy/broadcas_prac.py
@@ -4,3 +4,15 @@ A = np.array([1,2,3,4,5])
 
 result = A + 10
 print(result)
+
+print()
+print("Multiply matrix by scalar")
+A = np.array([[1,2,3],[4,5,6]])
+print(A * 5)
+
+print()
+print("Add two 1D arrays")
+A = np.array([1,2,3])
+B = np.array([4,5,6])
+print(A + B)
+
