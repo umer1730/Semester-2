9514b834607884b583d8fd2fff579a7a17afd06b
UMER YASEEN <umery1730@gmail.com>
2026-04-01 13:55:02 +0500

    create calculator by using name mangling

---
+++ Sem 3/CSV files/calculator.py
@@ -0,0 +1,17 @@
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def main():
+    print("Demo Mode")
+    print(add(5,3))
+    print(subtract(5,3))
+    print(multiply(5,3))
+
+if __name__ == "__main__":
+    main()
