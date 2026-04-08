7ae559c7de315760887ec7377da4832591fff0cc
UMER YASEEN <umery1730@gmail.com>
2026-04-08 08:42:17 +0500

    filter

--- Sem 3/Numpy/boolean_masks.py
+++ Sem 3/Numpy/boolean_masks.py
@@ -2,3 +2,9 @@ import numpy as np
 data = np.array([10,-5,20,-1])
 mask = data < 0
 print(mask)
+
+print()
+print("Filtering with masks")
+data = np.array([10,-5,20,-1])
+clean_data = data[data > 0]
+print(clean_data)
