e10eff8161482266abf53d342c7e8d0ebfd82c6f
UMER YASEEN <umery1730@gmail.com>
2026-04-10 17:24:34 +0500

    give comments

--- Sem 3/CSV files/process.py
+++ Sem 3/CSV files/process.py
@@ -1,11 +1,10 @@
 import csv
 
-with open("stud.csv","r",newline ="",encoding="utf-8") as f:
+with open("D:\\Sem 2\\Sem 3\\CSV files\\stud.csv","r",newline ="",encoding="utf-8") as f:
     reader = csv.reader(f)
 
     for row in reader:
-        print(row)
-
+        # print(row)
         City = row[2]
         if City == "Lahore":
             print(row)
