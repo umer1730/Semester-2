e85b1de446ea7c5416192554bfd28e18048346ae
UMER YASEEN <umery1730@gmail.com>
2026-04-10 17:16:55 +0500

    convert data to path

--- Sem 3/CSV files/lec1_csv.py
+++ Sem 3/CSV files/lec1_csv.py
@@ -1,6 +1,6 @@
 import csv
 
-with open("stud.csv","r",newline = "",encoding="utf-8") as f:
+with open("D:\\Sem 2\\Sem 3\\CSV files\\stud.csv","r",newline = "",encoding="utf-8") as f:
     reader = csv.reader(f)
 
     header = next(reader)
