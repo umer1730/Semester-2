b3789325f684edf553bc99de28c6e17fee99533d
UMER YASEEN <umery1730@gmail.com>
2026-04-22 14:33:10 +0500

    use filtering method to replace value using indexing

--- Sem 3/Numpy/find_greater.py
+++ Sem 3/Numpy/find_greater.py
@@ -4,3 +4,10 @@ np.random.seed(0)
 arr = np.random.randint(0,101,(10,10))
 result = arr[(arr % 2 == 0) & (arr > 50)]
 print(result)
+
+print()
+print("Replace value using indexing")
+arr = np.array([-5,0,7,-2,3,0,9])
+
+result = np.where(arr < 0,0,np.where(arr > 0,1,-1))
+print(result)
