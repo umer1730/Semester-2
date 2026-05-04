6a21022b88863e8e593edaf060dcdae427c713ff
UMER YASEEN <umery1730@gmail.com>
2026-05-04 10:04:59 +0500

    add

--- Sem 3/Numpy/vectorized_logic.py
+++ Sem 3/Numpy/vectorized_logic.py
@@ -5,3 +5,12 @@ data = np.array([10,-5,20,-1])
 result = np.where(data < 0,0,data)
 
 print(result)
+
+print()
+print("Patching data")
+raw_data = np.array([1,np.nan,3])
+backup = np.array([0,99,0])
+
+clean = np.where(np.isnan(raw_data),backup, raw_data)
+
+print(clean)
