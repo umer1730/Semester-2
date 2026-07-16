b93934ce8b342aba57feaaf68e19c69c680f6ba2
UMER YASEEN <umery1730@gmail.com>
2026-07-16 19:51:11 +0500

    add comments

--- lab 1/butter.py
+++ lab 1/butter.py
@@ -6,11 +6,9 @@ for i in range(1,n+1):
     print(" " * (2 * (n-i)), end="")
 
     print("*" * i)
-
+# loop
 for i in range(n,0,-1):
     print("*" * i,end="")
-
     print(" " * (2 * (n-i)),end="")
-
     print("*" * i)
     
